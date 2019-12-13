5e0004a09f89631433b07a5cec4fae257bdef03d
Herosbrine <gregoire.bezier@epitech.eu>
2019-12-13 12:02:44 +0100

    [ADD] | \n to sort list

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@ int main(int ac, char **argv)
     if (ac < 2)
         return (84);
     if (ac == 2) {
-        my_putchar(' ');
+        my_putchar('\n');
         exit (0);
     }
     pushswap((ac - 1), argv);
